734c2898d688f72ee398ca4662f5da4e98366b9e
Josue Rojas Ruiz <97050689+jrojas77@users.noreply.github.com>
2024-04-01 12:41:19 -0400

    Content base

---
+++ ObservableCollectionAddRange/ExtensionMethod.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObservableCollectionAddRange
+{
+    public static class ExtensionMethod
+    {
+        public static void AddRange<T>(this ObservableCollection<T> ObservableCollection, IEnumerable<T> items)
+        {
+            foreach (var item in items) 
+            {
+                ObservableCollection.Add(item);
+            }
+        }    
+    }
+}
